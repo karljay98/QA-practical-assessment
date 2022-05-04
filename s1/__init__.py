3d969c68665dccea2beb23c1fde3fde3c297bbb8
karljay98 <karljace@msn.com>
2022-05-04 09:47:05 +0000

    sudo code

--- s1/__init__.py
+++ s1/__init__.py
@@ -3,4 +3,5 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/transformer', methods=['GET'])
+@app.route('/transformer', methods=['GET', 'POST'])
+# a get request should be made for service 2 and 4
