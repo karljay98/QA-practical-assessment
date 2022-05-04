466d57963728ce23886cc48a05123e0ca8c97970
karljay98 <karljace@msn.com>
2022-05-04 09:37:14 +0000

    notes for service4

--- s4/__init__.py
+++ s4/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, Response
+import requests
 
 app == Flask(__name__)
 
-@app.route('get/transformer', methods =['GET', 'POST'])
+@app.route('get/transformer', methods =['POST'])
+# two get requests are needed for service 2 and 3
+# use these in logic for transformers.
 def get_transformer():
     
