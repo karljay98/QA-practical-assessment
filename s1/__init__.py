7fa17744015724e1b1d1d4dab8062f6fb08519fe
karljay98 <karljace@msn.com>
2022-05-05 22:12:20 +0000

    service1 logic

--- s1/__init__.py
+++ s1/__init__.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import requests
 
 app = Flask(__name__)
 
 @app.route('/transformer', methods=['GET', 'POST'])
 # a get request should be made for service 2 and 4
+def show_transformer():
+    s2 = request.get()
+    s3 = request.get()
+    transformer = s2+","+s3
+
+
+
+
+    return Response(transformer, mimetype='text/plain')
