da413c63b051c681026b2ffcae0f4e27fa2881e2
karljay98 <karljace@msn.com>
2022-05-17 11:37:20 +0000

    spelling mistake

--- s1/__init__.py
+++ s1/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/transformer', methods=['GET', 'POST'])
 # a get request should be made for service 2 and 4
 def show_transformer():
-    s2 = request.get('http://service2:5001/get/colour')
-    s3 = request.get('http://service3:5002/get/car')
+    s2 = requests.get('http://service2:5001/get/colour')
+    s3 = requests.get('http://service3:5002/get/car')
     transformer = s2+","+s3
 
 
