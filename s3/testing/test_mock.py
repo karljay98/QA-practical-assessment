f653316d5c1a21ef6f01ae7b8b992489c304f8d0
karljay98 <karljace@msn.com>
2022-05-17 22:10:13 +0000

    test written

---
+++ s3/testing/test_mock.py
@@ -0,0 +1,29 @@
+from unittest.mock import patch
+from flask import url_for, request
+from flask_testing import TestCase
+
+from s3 import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestService3(TestBase):
+    def test_mercendes(self):
+        with patch('random.randint') as r:
+            r.return_value = 0
+            response = self.client.get(url_for('cars'))
+            self.assertIn(b'Mercedes', response.data)
+
+    def test_mustang(self):
+        with patch('random.randint') as r:
+            r.return_value = 1
+            response = self.client.get(url_for('cars'))
+            self.assertIn(b'Mustang', response.data)
+
+    def test_mercendes(self):
+        with patch('random.randint') as r:
+            r.return_value = 2
+            response = self.client.get(url_for('cars'))
+            self.assertIn(b'Truck', response.data)
+
